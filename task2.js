ac1dcad9dad8c1a148e52a1de258a7295d8984d9
mehedi hasan <mehedihasan.j135@gmail.com>
2026-02-05 20:56:38 +0600

    task2 complete

---
+++ task2.js
@@ -0,0 +1,14 @@
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function oddEven(num) {
+  if (num % 2 === 0) {
+    const result = num / 2;
+    return result;
+  } else {
+    const result = num * 2;
+    return result;
+  }
+}
+
+console.log(oddEven(5));
